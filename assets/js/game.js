43d535bffe9b5b09379bb0d3df72e6d5b392fe79
kristinmoon <71245424+kristinmoon@users.noreply.github.com>
2020-10-07 23:51:14 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,6 @@ var fight = function (enemyName) {
   //repeat and execute as long as the enemy robot is alive
   while (enemyHealth > 0 && playerHealth > 0) {
 
-    // Alert users that they are starting the round
-    // window.alert("Welcome to Robot Gladiators!");
-
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if user picks "skip" confirm and then stop the loop
@@ -75,8 +72,51 @@ var fight = function (enemyName) {
   }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
-  var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
-  fight(pickedEnemyName);
-}
+// function to start a new game
+var startGame = function () {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+  }
+  // after the loop ends, player is either out of health or enemiesto fight, so run endGame function
+  endGame();
+};
+
+// function to end the entire game
+var endGame = function () {
+  //if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  //ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    //restart the game
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
+
+
+//start game when page loads
+startGame();
